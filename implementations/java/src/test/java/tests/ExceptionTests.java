d6a0056f4c05c89750e98774ae7bcbf93e57db88
GregJohnStewart <contact@gjstewart.net>
2017-10-21 14:20:11 -0400

    Made the Exception tests fancier and more automated.

--- implementations/java/src/test/java/tests/ExceptionTests.java
+++ implementations/java/src/test/java/tests/ExceptionTests.java
@@ -10,7 +10,10 @@ import com.ebp.owat.lib.datastructure.node.value.OwatNodeValueException;
 import com.ebp.owat.lib.datastructure.set.OwatNodeSetException;
 import com.ebp.owat.lib.utils.OwatUtilException;
 import com.ebp.owat.lib.utils.rand.OwatRandException;
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,10 +23,13 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Theories.class)
 public class ExceptionTests {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionTests.class);
 	
-	private List<Class<? extends OwatException>> exceptionsToTest = Arrays.asList(
+	/** The different exceptions to test. */
+	@DataPoints
+	public static List<Class<? extends OwatException>> exceptionsToTest = Arrays.asList(
 			OwatException.class,
 			OwatStructureException.class,
 			OwatNodeIOException.class,
@@ -39,30 +45,24 @@ public class ExceptionTests {
 	private static final Throwable TEST_THROWABLE = new Throwable("Test throwable message");
 	private static final String TEST_MESSAGE = "TEST MESSAGE FOR EXCEPTION TESTING";
 	
-	@Test
-	public void testExceptions() throws Throwable{
-		LOGGER.info("Testing all the custom exceptions (mainly for coverage...).");
+	@Theory
+	public void testExceptions(Class<? extends OwatException> curClass) throws Throwable{
+		LOGGER.debug("Testing: {}", curClass.getName());
+		Constructor<? extends OwatException> constBase = curClass.getConstructor();
+		Constructor<? extends OwatException> constStr = curClass.getConstructor(String.class);
+		Constructor<? extends OwatException> constThro = curClass.getConstructor(Throwable.class);
+		Constructor<? extends OwatException> constStrThro = curClass.getConstructor(String.class, Throwable.class);
 		
-		for (Class<? extends OwatException> curClass : exceptionsToTest ) {
-			LOGGER.debug("Testing: {}", curClass.getName());
-			Constructor<? extends OwatException> constBase = curClass.getConstructor();
-			Constructor<? extends OwatException> constStr = curClass.getConstructor(String.class);
-			Constructor<? extends OwatException> constThro = curClass.getConstructor(Throwable.class);
-			Constructor<? extends OwatException> constStrThro = curClass.getConstructor(String.class, Throwable.class);
-			
-			OwatException curException = constBase.newInstance();
-			
-			curException = constStr.newInstance(TEST_MESSAGE);
-			assertEquals(TEST_MESSAGE, curException.getMessage());
-			
-			curException = constThro.newInstance(TEST_THROWABLE);
-			assertEquals(TEST_THROWABLE, curException.getCause());
-			
-			curException = constStrThro.newInstance(TEST_MESSAGE, TEST_THROWABLE);
-			assertEquals(TEST_MESSAGE, curException.getMessage());
-			assertEquals(TEST_THROWABLE, curException.getCause());
-		}
+		OwatException curException = constBase.newInstance();
 		
+		curException = constStr.newInstance(TEST_MESSAGE);
+		assertEquals(TEST_MESSAGE, curException.getMessage());
+		
+		curException = constThro.newInstance(TEST_THROWABLE);
+		assertEquals(TEST_THROWABLE, curException.getCause());
+		
+		curException = constStrThro.newInstance(TEST_MESSAGE, TEST_THROWABLE);
+		assertEquals(TEST_MESSAGE, curException.getMessage());
+		assertEquals(TEST_THROWABLE, curException.getCause());
 	}
-
 }
